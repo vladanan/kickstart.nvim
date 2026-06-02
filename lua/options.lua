e5eeab4a46ba79d6706c9b5a68659cc177f2d9b4
vladanan <vladan.andjelkovic@gmail.com>
2026-06-02 13:02:40 +0200

    search

--- lua/options.lua
+++ lua/options.lua
@@ -15,7 +15,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
